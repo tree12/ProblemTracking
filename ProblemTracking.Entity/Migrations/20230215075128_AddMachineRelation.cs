42c33c785067e7c947e68ae1998c90fc79bfedfb
tree12 <greentree_072@hotmail.com>
2023-02-21 15:17:11 +0700

    Add machine relationship

---
+++ ProblemTracking.Entity/Migrations/20230215075128_AddMachineRelation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProblemTracking.Entity.Migrations
+{
+    public partial class AddMachineRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.AddColumn<int>(
+                name: "MachineId",
+                schema: "dbo",
+                table: "Problem",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Problem_MachineId",
+                schema: "dbo",
+                table: "Problem",
+                column: "MachineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Problem_Machine_MachineId",
+                schema: "dbo",
+                table: "Problem",
+                column: "MachineId",
+                principalSchema: "dbo",
+                principalTable: "Machine",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.Sql(@"update Problem set MachineId ='1'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Problem_Machine_MachineId",
+                schema: "dbo",
+                table: "Problem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Problem_MachineId",
+                schema: "dbo",
+                table: "Problem");
+
+            migrationBuilder.DropColumn(
+                name: "MachineId",
+                schema: "dbo",
+                table: "Problem");
+
+           
+        }
+    }
+}
